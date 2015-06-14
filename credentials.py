73e40aee35230b544311b23b19a444cb229afdcf
yurka <bushnevyuri91@gmail.com>
2015-06-15 00:27:28 +0200

    Initial commit

---
+++ credentials.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["OS_TENANT_NAME"] = "admin"
+os.environ["OS_USERNAME"] = "admin"
+os.environ["OS_PASSWORD"] = "openstack"
+os.environ["OS_AUTH_URL"] = "http://controller:35357/v2.0"
+
+
