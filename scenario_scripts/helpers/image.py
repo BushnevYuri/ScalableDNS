0ca54be54b5d626af96a5dd977f789e197b8a6c7
BushnevYuri <bushnevyuri91@gmail.com>
2015-06-18 17:25:46 +0200

    create image script

---
+++ scenario_scripts/helpers/image.py
@@ -0,0 +1,17 @@
+import admin_credentials
+import keystoneclient.v2_0.client as ksclient
+import glanceclient
+import glanceclient.v2.client as glclient
+from services_credentials import get_keystone_creds
+
+def create_image(image_name, image_location, disk_format, container_format):
+    creds = get_keystone_creds()
+    keystone = ksclient.Client(**creds)
+    glance_endpoint = keystone.service_catalog.url_for(service_type='image',
+                                                   endpoint_type='publicURL')
+    glance = glanceclient.Client('1',glance_endpoint, token=keystone.auth_token) # first arg is version
+    with open(image_location) as fimage:
+        glance.images.create(name=image_name, is_public=True, disk_format=disk_format, container_format=container_format, data=fimage)
+
+if __name__ == '__main__':
+    create_image('ubuntu', '/home/stack/Downloads/precise-server-cloudimg-amd64-disk1.img', 'qcow2', 'bare')
--- settings/credentials/services_credentials.py
+++ settings/credentials/services_credentials.py
@@ -17,6 +17,15 @@ def get_neutron_creds():
     d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
 
+def get_keystone_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['tenant_name'] = os.environ['OS_TENANT_NAME']
+    return d
+
+
 def get_token():
     keystone = client.Client(username=os.environ['OS_USERNAME'],password=os.environ['OS_PASSWORD'],tenant_name=os.environ['OS_TENANT_NAME'],auth_url=os.environ['OS_AUTH_URL'])
     token_dict = keystone.auth_ref['token']['id']
