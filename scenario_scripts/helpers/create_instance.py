a57a1550360a78217973b8373c0369ba87f0795a
BushnevYuri <bushnevyuri91@gmail.com>
2015-06-19 14:17:59 +0200

    ssh key generation  for instance

--- scenario_scripts/helpers/create_instance.py
+++ scenario_scripts/helpers/create_instance.py
@@ -10,16 +10,23 @@ def create_instance(instance_name,image_name, flavor, security_group, networks):
     creds = get_nova_creds()
     nova = nclient.Client(**creds)
 
-    if not nova.keypairs.findall(name='demo-key'):
-          with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
-              public_key = fpubkey.read()
-              nova.keypairs.create(name='demo-key', public_key=fpubkey.read())
+    keypair = None
+    key_name = 'demo-key9'
+
+    openstack_keys_location = "/tmp/openstack_keys/"
+    try:
+        os.stat(openstack_keys_location)
+    except:
+        os.mkdir(openstack_keys_location)
+    if not nova.keypairs.findall(name=key_name):
+          os.system("nova keypair-add %s > %s/%s.pem" % (key_name, openstack_keys_location, key_name))
+          os.system("chmod 600 %s/%s.pem" % (openstack_keys_location, key_name))
 
     image = nova.images.find(name=image_name)
     flavor = nova.flavors.find(name=flavor)
-    group = nova.security_groups.find(name=security_group)
-
-    instance = nova.servers.create(name=instance_name, image=image, flavor=flavor, key_name='demo-key', nics = network)
+    group = nova.security_groups.find(name=security_group).name
+    groups = [group]
+    instance = nova.servers.create(name=instance_name, image=image, flavor=flavor, key_name=key_name, nics = network, security_groups=groups)
 
     #Poll at 5 second intervals, until the status is no longer 'BUILD'
     status = instance.status
@@ -39,12 +46,14 @@ def create_instance(instance_name,image_name, flavor, security_group, networks):
                                ip_protocol='icmp',
                                from_port=-1,
                                to_port=-1)
-
+    print 'Security group rules successfully added'
 
     nova.floating_ips.list()
     floating_ip = nova.floating_ips.create()
-    instance = nova.servers.find(name='Ubuntu')
+    instance = nova.servers.find(name='Ubuntu test')
     instance.add_floating_ip(floating_ip)
+
+    print "Floating ip %s added to instance" % floating_ip.ip
 
 
 
@@ -56,6 +65,6 @@ def remove_instance(instance_name):
     print 'Image removed'
 
 if __name__ == '__main__':  #TODO remove after testing
-    create_instance('Ubuntu test','ubuntu_server','m1.small','default','da83df25-6e68-42ca-a656-2aad2f00fcd8')
+    create_instance('Ubuntu test','ubuntu_server','m1.small','default','e5a9c538-9d94-4738-ad3c-87040685692a')
     #time.sleep(10)
     #remove_instance('Ubuntu test')
