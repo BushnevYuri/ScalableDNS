fc87c600ccde583039e15a53dbc8fb8f050d8910
BushnevYuri <bushnevyuri91@gmail.com>
2015-08-31 15:26:45 +0200

    sycn up impl

--- scenario_scripts/scalable_dns/nfv_resilence_POC.py
+++ scenario_scripts/scalable_dns/nfv_resilence_POC.py
@@ -1,14 +1,12 @@
-import time
 import paramiko
 from instance import *
 from subnet import *
 
-
-#####  RUN sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE         to get internet connection in openstack instances!!!!!
-
-EXTERNAL_NETWORK_ID = '0028da9b-6933-4d4b-b83d-e0476be1e34c'
-
 ########  Variables  #########
+
+#Need to change value after each stack rerun ('how to' check in video)
+EXTERNAL_NETWORK_ID = '62222b3b-51f2-432c-9248-f82fe717c099'
+
 
 NETWORK_NAME = 'dns_network'
 SUBNET_NAME = 'dns_network'
@@ -32,6 +30,10 @@ SUBNETS_PER_SERVICE_NODE = 1
 ##############################
 
 def purge_dns(router_id, healthmonitor_id, instances_list=DNS_SERVERS):
+    """
+    If something goes wrong, this function will clean whole environment after failure
+    :return:
+    """
     remove_load_balancer_virtual_ip(LOAD_BALANCER_VIP)
     remove_load_balancer(LOAD_BALANCER_POOL_NAME, healthmonitor_id)
     for instance in instances_list:
@@ -43,6 +45,9 @@ def purge_dns(router_id, healthmonitor_id, instances_list=DNS_SERVERS):
     remove_router(router_id)
 
 def get_number_of_subnets_for_dns_vip(dns_vip_ip):
+    """
+    :return: Number of subnets which is managed by one dns with specified ip
+    """
     subnets_count = 0
     subnets = get_list_of_subnets()
     for subnet in subnets['subnets']:
@@ -58,17 +63,18 @@ def add_additional_dns_node(subnet_id, image_name, sync_up = False):
     instance_name = 'dns_%s' % time.time()
     instance_ip, floating_ip, openstack_key_file = create_instance(instance_name, image_name, DNS_SERVERS_FLAVOR, DNS_SECURITY_GROUP, subnet_id, SUBNET_NAME)
     DNS_SERVERS.append(instance_name)
-    add_member_to_load_balancer_pool (instance_ip, LOAD_BALANCER_POOL_NAME, LOAD_BALANCER_PORT)
+    add_member_to_load_balancer_pool (instance_ip, LOAD_BALANCER_POOL_NAME, LOAD_BALANCER_PORT) #add created instance to load balancer pool
 
     key = paramiko.RSAKey.from_private_key_file(openstack_key_file)
     ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # to avoid any user interactions while ssh login
 
     print "Instance %s was successfully added" % instance_name
     for x in range(20):
         try:
             ssh.connect(floating_ip.ip, username=CLOUD_IMAGE_USERNAME, pkey=key, timeout=300)
             print 'connected'
+            #TODO Need to add script for simple web service which will have some state
             #ssh.exec_command('touch %s' % instance_name)
             #ssh.exec_command('sudo python -m SimpleHTTPServer 80 > /dev/null 2>&1 &')
 
@@ -81,16 +87,14 @@ def add_additional_dns_node(subnet_id, image_name, sync_up = False):
 
 
 
-
-if __name__ == '__main__':  #TODO remove after testing
-    #purge_dns('58d52cb9-72be-40ff-a8ca-5f104d0bc9eb', 'ee50e206-d1f4-447c-bf74-8449f02933ad',['c41b6b11-d439-4e40-8100-f80954f73667','0ebb7743-6f32-4bf4-a1b3-1eace3e1eae5','4e5deee4-674c-4696-bd30-9b8a82dcc8c8','aa12cdf4-d504-4a24-abef-5d684cf4f99b'])
+if __name__ == '__main__':
     try:
         print 'Starting dns...'
         router_id = create_router(BASE_ROUTER, EXTERNAL_NETWORK_ID)
         'Dns router was added, id: %s' % router_id
         subnet_id = create_subnet(NETWORK_NAME, SUBNET_NAME, SUBNET_ADDRESS, interface_router_id=router_id)
         load_balaner_pool_id, healthmonitor_id = create_load_balancer(LOAD_BALANCER_POOL_NAME, LOAD_BALANCER_METHOD, LOAD_BALANCER_PROTOCOL, SUBNET_NAME)
-        ip = create_load_balancer_virtual_ip(LOAD_BALANCER_VIP, LOAD_BALANCER_PORT, LOAD_BALANCER_PROTOCOL, SUBNET_NAME, LOAD_BALANCER_POOL_NAME)
+        ip = create_load_balancer_virtual_ip(LOAD_BALANCER_VIP, LOAD_BALANCER_PORT, LOAD_BALANCER_PROTOCOL, SUBNET_NAME, LOAD_BALANCER_POOL_NAME) # to have one entry point for all NFV members
         print 'VIP for load dns load balancer: %s' % ip
         ret_val  = add_additional_dns_node(subnet_id, "NFV_instance1")
         floating_ip = ret_val[0]
@@ -100,15 +104,18 @@ if __name__ == '__main__':  #TODO remove after testing
         sync = None
         sync_up_ip = None
         while (True):
-             time.sleep(5)
+             time.sleep(20)  #should be ~several secs in production. Each iteration we check if there is a need to create new NFV instance and also perform state sync up in existing
 
              if (sync):
                 ssh = paramiko.SSHClient()
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 ssh.connect(floating_ip.ip, username=CLOUD_IMAGE_USERNAME, pkey=key, timeout=300)
+
+                #FIRST BREAKPOINT
                 ssh.exec_command('sudo unison -auto -batch /tmp ssh://'+ sync_up_ip.ip +'//tmp')
                 ssh.close()
 
+             #SECOND BREAKPOINT
              subnets_count = get_number_of_subnets_for_dns_vip(ip)
              if DNS_SERVERS.__len__() * SUBNETS_PER_SERVICE_NODE <= subnets_count:
                  ret_val = add_additional_dns_node(subnet_id, "NFV_instance2")
@@ -116,7 +123,7 @@ if __name__ == '__main__':  #TODO remove after testing
                  sync = True
 
     finally:
-        print "Teardown"
+        print "Teardown" # if something goes wrong this step just clean all environment to avoid manual clean up each time
         purge_dns(router_id, healthmonitor_id)
 
 
--- scenario_scripts/scalable_dns/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-openstack_key_file = "/tmp/openstack_keys/some_key3.pem"
-floating_ip = "172.24.4.11"
-
-#os.system("scp -i " + openstack_key_file + " /home/stack/.ssh/id_rsa.pub ubuntu@" + floating_ip + ":/home/ubuntu/.ssh")
-
-os.system("ssh-copy-id -i /home/stack/.ssh/id_rsa.pub ubuntu@" + floating_ip)
-
